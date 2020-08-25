19ff058358bf66744e5ad35afd938a953c7e01c8
randy alcala <r.alcala.h@gmail.com>
2020-08-25 15:45:22 -0500

    funciones js agregadas: cargaPantalla, comienzaJuego, ClickTarjeta, efectoVolteartarjeta

---
+++ js/script.js
@@ -0,0 +1,86 @@
+class Memorama {
+
+    constructor() {
+        
+        this.totalTarjetas = [];
+        this.numeroTarjetas = 0;
+        this.verificadorTarjetas = [];
+        this.errores = 0;
+        this.nivelDifivultad = '';
+        this.imagenesCorrectas = [];
+        this.agregadorTarjetas = [];
+
+        this.$contenedorGeneral = document.querySelector('.contenedor-general');
+        this.$contenedorTarjetas = document.querySelector('.contenedor-tarjetas');
+        this.$pantallaBloqueada = document.querySelector('.pantalla-bloqueada')
+        this.$mensaje = document.querySelector('h2.mensaje');        
+
+        //Llamado a los eventos
+        this.eventos();
+    }
+
+    eventos() {
+        window.addEventListener('DOMContentLoaded', () => {
+            this.cargaPantalla();
+
+        });
+    }
+
+    async cargaPantalla() {
+        const respuesta = await fetch('../memo.json');        
+        const data = await respuesta.json();
+        this.totalTarjetas = data;
+        if(this.totalTarjetas.length > 0) {
+            this.totalTarjetas.sort(orden);
+            function orden(a,b) {
+                return Math.random() - 0.5;
+            }
+        }
+        
+        this.numeroTarjetas = this.totalTarjetas.length;
+
+        let html = '';
+        this.totalTarjetas.forEach(card => {
+            html += `<div class="tarjeta">
+                        <img class="tarjeta-img" src=${card.src} alt="imagen memorama">
+                    </div>`
+        });
+
+        this.$contenedorTarjetas.innerHTML = html; 
+        this.comienzaJuego();       
+    }
+
+    comienzaJuego() {
+        const tarjetas = document.querySelectorAll('.tarjeta');
+        tarjetas.forEach(tarjeta => {
+            tarjeta.addEventListener('click', e => {
+                this.clickTarjeta(e);
+            })
+        });
+    }
+
+    clickTarjeta(e) {
+        this.efectoVoltearTarjeta(e);
+
+        let sourceImage = e.target.childNodes[1].attributes[1].value;
+        this.verificadorTarjetas.push(sourceImage);
+
+        let tarjeta = e.target;        
+        this.agregadorTarjetas.unshift(tarjeta);
+        this.comparadorTarjetas();
+    }
+
+    efectoVoltearTarjeta(e) {
+        e.target.style.backgroundImage = 'none';
+        e.target.style.backgroundColor = 'white';
+        e.target.childNodes[1].style.display = 'block';
+    }
+
+    comparadorTarjetas() {  
+
+    }
+}
+
+new Memorama();
+
+//minuto 6:46 video 4
